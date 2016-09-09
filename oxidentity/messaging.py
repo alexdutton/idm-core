980fff058f3132b19ea9ff0173a6fac018c07cac
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2016-09-09 10:37:12 +0100

    Sync all reference data to a 'reference' AMQP exchange, and use a connection pool

--- oxidentity/messaging.py
+++ oxidentity/messaging.py
@@ -8,9 +8,16 @@ from django.conf import settings
 from django.db import connection
 from django.db.models.signals import post_save, post_delete, pre_delete
 
+from oxidentity.gender.models import Gender
+from oxidentity.gender.serializers import GenderSerializer
+from oxidentity.identifier.models import IdentifierType
+from oxidentity.identifier.serializers import IdentifierTypeSerializer
 from oxidentity.models import Person
-from oxidentity.org_relationship.models import Affiliation, Role
-from oxidentity.org_relationship.serializers import AffiliationSerializer, RoleSerializer
+from oxidentity.nationality.models import Country
+from oxidentity.nationality.serializers import CountrySerializer
+from oxidentity.org_relationship.models import Affiliation, Role, AffiliationType, RoleType, Unit
+from oxidentity.org_relationship.serializers import AffiliationSerializer, RoleSerializer, RoleTypeSerializer, \
+    AffiliationTypeSerializer, UnitSerializer
 from oxidentity.serializers import PersonSerializer
 
 INITIAL_FIELD_VALUES = '_initial_field_values'
@@ -18,24 +25,41 @@ NEEDS_PUBLISH = '_needs_publish'
 
 _ModelConfig = collections.namedtuple('_ModelConfig', 'serializer exchange natural_key')
 
+reference_exchange = kombu.Exchange('iam.idm.reference', 'topic', durable=True)
+
 _model_config = {
     Person: _ModelConfig(PersonSerializer,
                          kombu.Exchange('iam.idm.person', 'topic', durable=True),
-                         'uuid'),
+                         lambda instance: instance.uuid),
     Affiliation: _ModelConfig(AffiliationSerializer,
                               kombu.Exchange('iam.idm.affiliation', 'topic', durable=True),
-                              'person_id'),
+                              lambda instance: instance.person_id),
     Role: _ModelConfig(RoleSerializer,
                        kombu.Exchange('iam.idm.role', 'topic', durable=True),
-                       'person_id'),
+                       lambda instance: instance.person_id),
 }
 
+reference_models = [
+    (AffiliationType, AffiliationTypeSerializer),
+    (RoleType, RoleTypeSerializer),
+    (IdentifierType, IdentifierTypeSerializer),
+    (Gender, GenderSerializer),
+    (Country, CountrySerializer),
+    (Unit, UnitSerializer),
+]
+
+for model, serializer in reference_models:
+    _model_config[model] = _ModelConfig(serializer, reference_exchange,
+                                        lambda instance: '{}.{}'.format(type(instance).__name__, instance.pk))
 
 class _FakeRequest(object):
     def build_absolute_uri(self, url):
         return urljoin(settings.API_BASE, url)
 
     GET = {}
+
+
+amqp_connection = kombu.Connection(settings.BROKER_URL)
 
 
 def publish_model_change_to_amqp(sender, instance, **kwargs):
@@ -55,13 +79,14 @@ def publish_model_change_to_amqp(sender, instance, **kwargs):
 
     serializer = model_config.serializer(context={'request': _FakeRequest()})
 
-    with kombu.Connection(settings.BROKER_URL) as conn:
-        producer = conn.Producer(serializer='json')
+    with kombu.producers[amqp_connection].acquire(block=True) as producer:
         producer.publish(serializer.to_representation(instance),
                          exchange=model_config.exchange,
                          routing_key='{}.{}'.format(publish_type,
-                                                    getattr(instance, model_config.natural_key)),
+                                                    model_config.natural_key(instance)),
                          declare=[model_config.exchange])
+
+
 
 
 def instance_changed(sender, instance, created, **kwargs):
