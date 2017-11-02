709f551b4fb8243b974bf6c8d231a33d9fa29225
Christopher Hoskin <mans0954@debian.org>
2017-11-02 21:53:12 +0000

    Allow database user to be set from environment variable

--- idm_core/settings.py
+++ idm_core/settings.py
@@ -30,6 +30,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DATABASE_NAME', 'idm_core'),
         'HOST': os.environ.get('DATABASE_HOST', 'localhost')
+        'USER': os.environ.get('DATABASE_USER', 'postgres')
     },
 }
 
